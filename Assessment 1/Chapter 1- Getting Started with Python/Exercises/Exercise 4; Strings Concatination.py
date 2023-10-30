a76a4db715d889f1eb925bc097fc2506721faabf
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-10-30 04:29:14 +0400

    Update Exercise 4; Strings Concatination.py

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4; Strings Concatination.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4; Strings Concatination.py
@@ -1,10 +1,10 @@
-from math import pi
+# Define three based on the description you would type in the strings
+string01 = "Hello, "  # First string
+string02 = "I am Andrea Cyra Valenzuela Bautista. "  # Second string
+string03 = "It's nice to meet you."  # Third string
 
-# Accept the radius of the circle from the user
-radius = float(input("Enter the radius of the circle: "))
+# Concatenate or join the three strings together
+MultipleStrings = string01 + string02 + string03
 
-# Calculate the area of the circle
-area = pi * radius * radius
-
-# Print the computed area
-print("The area of the circle with radius", radius, "is", area)
+# Print the concatenated string, this would print into one line of string.
+print(MultipleStrings)
