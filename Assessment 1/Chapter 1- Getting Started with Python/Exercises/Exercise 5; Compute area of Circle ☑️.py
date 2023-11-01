9db1005873da5557610b4869451e5908c4082642
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-11-01 20:40:45 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 5; Compute area of Circle ☑️.py
@@ -0,0 +1,11 @@
+# Import the math module to access mathematical functions and constants
+import math
+
+# Prompt the user to enter the radius of the circle and convert it to a float
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle using the formula A = πr^2
+area = math.pi * radius * radius
+
+# Display the calculated area of the circle by print
+print("Area of the circle =", area)
