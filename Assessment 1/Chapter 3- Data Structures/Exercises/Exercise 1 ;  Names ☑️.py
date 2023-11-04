aca81bc0464a3c78c4afa437ff602c1c4b0ae9ce
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-11-04 15:45:08 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 1 ;  Names ☑️.py
@@ -0,0 +1,6 @@
+# Create a list of your friends' names
+list_names = ["Lorac", "Yenna", "Adeia", "Diwa", "Escanor"]
+
+# Iterate through the list and print each name
+for name in list_names:
+    print(name)
---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 2 ;Greetings ☑️.py
@@ -0,0 +1,7 @@
+# Create a list of your friends' names
+list_names = ["Lorac", "Yenna", "Adeia", "Diwa", "Escanor"]
+
+# Iterate through the list and print a personalized message to each person
+for name in list_names:
+    message = f"Hello, {name}! I hope you're doing well."
+    print(message)
