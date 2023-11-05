8b7f6d0c4386ca91338aa4c1b1958f31b71545d6
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-11-05 11:04:08 +0400

    Update Exercise 4 ; Guest List ☑️.py

--- Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 4 ; Guest List ☑️.py
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 4 ; Guest List ☑️.py
@@ -6,7 +6,7 @@ event_date = "November 7, 2023"
 event_location = "The Library of Congress, Washington, D.C"
 invitor = "The Host Committee of Knowledge Society"
 
-# Create a loop to repeatedly ask the user for names and display invitations
+# Create a loop to ask the user for names and display invitations repeatedly
 while True:
     guest_name = input("Please enter a name on the list (or 'q' to quit): ")
     
@@ -18,4 +18,4 @@ while True:
         invitation_message = f"Dear {guest_name},\n\nI am writing to extend a formal invitation to you for a dinner event. Your presence would be a great honor and privilege.\n\nThe dinner will take place on {event_date} at {event_location}. We anticipate an evening of engaging conversation and camaraderie.\n\nKindly confirm your attendance at your earliest convenience and let us know of your availability.\n\nWe are looking forward to your positive response and to spending a memorable evening together.\n\nSincerely,\n{invitor}\n"
         print(invitation_message)
     else:
-        print("The entered name is not on the list, please Try Again")
+        print("The entered name is not on the list.")
