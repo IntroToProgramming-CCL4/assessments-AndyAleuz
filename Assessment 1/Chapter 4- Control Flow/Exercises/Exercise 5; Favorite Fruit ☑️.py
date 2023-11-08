7f97f0b71b8ce0eac8036ad94755fd36de1492b4
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-11-08 20:55:15 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 5; Favorite Fruit ☑️.py
@@ -0,0 +1,21 @@
+# Creating a list of favorite fruits
+favorite_fruits = ["Rambutan", "Aratilis", "Jackfruit", "Star apple", "Papaya", "Dragon fruit"]
+
+# Make a list of your three favorite fruits
+my_favorites = ["Rambutan", "Papaya", "Dragon fruit"]
+
+# Check if each fruit in your list is a favorite
+if "Rambutan" in my_favorites:
+    print("You really like Rambutan!")
+
+if "Aratilis" in my_favorites:
+    print("You really like Aratilis!")
+
+if "Jackfruit" in my_favorites:
+    print("You really like Jackfruit!")
+
+if "Star apple" in my_favorites:
+    print("You really like Star apple!")
+
+if "Papaya" in my_favorites:
+    print("You really like Papaya!")
