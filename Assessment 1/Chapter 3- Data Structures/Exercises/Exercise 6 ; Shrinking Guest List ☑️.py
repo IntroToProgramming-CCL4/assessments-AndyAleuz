075ae1ad13ba7e6eae849c77273a2260a48acd72
AndyAleuz <144931071+AndyAleuz@users.noreply.github.com>
2023-11-08 20:52:32 +0400

    Update Exercise 6 ; Shrinking Guest List ☑️.py

--- Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 6 ; Shrinking Guest List ☑️.py
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise 6 ; Shrinking Guest List ☑️.py
@@ -11,16 +11,16 @@ print("Dear Guests, due to limited seating, we can only invite two people for di
 
 # Use pop() to remove guests from your list one at a time until only two names remain
 while len(guests) > 2:
-    guest = guests.pop()
-    print(f"Regrettably, {guest}, we can't invite you to the dinner this time. We hope to see you at future events.\n")
+    removed_guest = guests.pop()
+    print(f"Regrettably, {removed_guest}, we can't invite you to the dinner this time. We hope to see you at future events.\n")
 
-# Print a message to the two people still on your list
+# Print invitations for the two remaining guests
 print("Invitations have been extended to the following guests:\n")
 for guest in guests:
-    invitation_message = f"Dear {guest},\n\nYou are cordially invited to the dinner event. Your presence would be a great honor and privilege. The dinner will take place on {event_date} at {event_location}. We anticipate an evening of engaging conversation and camaraderie. Kindly confirm your attendance at your earliest convenience and let us know of your availability. We are looking forward to your positive response and to spending a memorable evening together.\nSincerely,\n{invitor}\n"
+    invitation_message = f"Dear {guest},\n\nYou are cordially invited to the dinner event on {event_date} at {event_location}. We anticipate an evening of engaging conversation and camaraderie. Kindly confirm your attendance at your earliest convenience. We are looking forward to your positive response.\nSincerely,\n{invitor}\n"
     print(invitation_message)
 
-# Use del to remove the other names from your list
+# Use del to remove any extra names from your list
 del guests[2:]
 
 # Print the final guest list with only two names
